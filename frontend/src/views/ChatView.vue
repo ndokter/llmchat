b85bdb30df28bec6aeeb48c0be1ae4e695395a77
Nigel Dokter <mail@nldr.net>
2025-10-05 14:17:29 +0200

    frontend: sidebar styling improvement

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -5,12 +5,29 @@ import Chat from '../components/Chat.vue'
 
 <template>
   <div class="layout">
-    <SideBar />
-    <Chat />
+    <div class="sidebar-wrapper">
+      <SideBar />
+    </div>
+    <div class="chat-wrapper">
+      <Chat />
+    </div>
   </div>
 </template>
 
 <style scoped>
+.layout {
+  display: flex;
+  height: 100vh;
+}
 
+.sidebar-wrapper {
+  width: 200px;
+  flex: 0 0 200px;
+  background: #140079ff;
+}
+
+.chat-wrapper {
+  flex: 1;
+  overflow: hidden;
+}
 </style>
-
